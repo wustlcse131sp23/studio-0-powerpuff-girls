2ad508e1937444819951d9ac4c50b7c11dcca234
jennymann <jennymann@localhost>
2023-01-19 13:32:03 -0600

    lavander matcha latte

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -5,5 +5,6 @@ public class Message {
 	public static void main(String args[]) {
 		System.out.println("Say hi to your group mates!");
 		System.out.println("I love the powerpuff girls and professor X");
+		System.out.println("i miss my lavander matcha latte :(");
 	}
 }
